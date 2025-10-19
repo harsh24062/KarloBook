895713342b249c4623efd680cd71319a0ebac679
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-10-19 18:14:40 +0530

    Added Assets, Routes and Toaster in App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import {Route, Routes, useLocation} from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favorite from './pages/Favorite'
+import {Toaster} from 'react-hot-toast'
 
 function App() {
   
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
   return (
-    <div>
-     KarloBook
-    </div>
+   <>
+   <Toaster />
+   {!isAdminRoute && <NavBar />}
+   <Routes>
+     <Route path='/' element={<Home />}/>
+     <Route path='/movies' element={<Movies />}/>
+     <Route path='/movies/:id' element={<MovieDetails />}/>
+     <Route path='/movies/:id/:date' element={<SeatLayout />}/>
+     <Route path='/my-bookings' element={<MyBookings />}/>
+     <Route path='/favorite' element={<Favorite />}/>
+   </Routes>
+    {!isAdminRoute && <Footer />}
+   </>
   )
 }
 
