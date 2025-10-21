2794f3e789cce941760d3191422336e5717675bc
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-10-21 12:20:00 +0530

    Added User Authentication using Clerk and added my-bookings option in clerk menu

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import {assets} from "../assets/assets"
-import {MenuIcon, SearchIcon, XIcon} from "lucide-react"
+import {MenuIcon, SearchIcon, TicketPlus, XIcon} from "lucide-react"
 import { useState } from "react"
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react"
 
 const NavBar = () => {
 
   const [isOpen,setIsOpen] = useState(false)
+  const {user} = useUser()
+  const {openSignIn} = useClerk()  // openSignIn is in-build
+
+  const navigate = useNavigate()
 
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-14 lg:px-20 py-0">
@@ -30,10 +35,23 @@ const NavBar = () => {
         {/* Login button and Search icon */}
         <div className="flex items-center gap-8">
            <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer"/>
-           <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull 
-             transition rounded-full font-medium cursor-pointer">
-             LogIn
-            </button>      
+           {!user?(
+             <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull 
+                transition rounded-full font-medium cursor-pointer" 
+                onClick={openSignIn}>
+                LogIn
+              </button> 
+             ):(
+                // User Profile will display 
+                <UserButton>
+                  {/* Adding my-bookings button option in clerk menu */}
+                  <UserButton.MenuItems>
+                    <UserButton.Action label="My Bookings" labelIcon={<TicketPlus width={15}/>} 
+                    onClick={()=>navigate('/my-bookings')}/>
+                  </UserButton.MenuItems>
+                </UserButton>
+              )
+            }      
         </div>
         {/* Menu Icon */}
         <MenuIcon className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" onClick={()=>{setIsOpen(!isOpen)}}/>
