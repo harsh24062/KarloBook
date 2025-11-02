ab1a700b0b77bf0a393a13d6446626d69daea8ab
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-11-02 22:31:25 +0530

    Added Trailer Section in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import FeaturedSection from "../components/FeaturedSection"
 import HomeFront from "../components/HomeFront"
+import TrailersSection from "../components/TrailersSection"
 
 const Home = () => {
   return (
     <>
       <HomeFront />
       <FeaturedSection />
+      <TrailersSection />
     </>
   )
 }
