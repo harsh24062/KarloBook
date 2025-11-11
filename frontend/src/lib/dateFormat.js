e9e6b2e094bce5d50d53fa35bc4218edadc4481d
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-11-11 22:09:42 +0530

    Added My-Booking  Page

---
+++ frontend/src/lib/dateFormat.js
@@ -0,0 +1,11 @@
+
+const dateFormat = (dateString) => {
+   const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  });
+}
+
+export default dateFormat
--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -1,7 +1,57 @@
+import { useEffect, useState } from "react"
+import { dummyBookingData } from "../assets/assets"
+import Loading from "../components/Loading"
+import calcTime from "../lib/calcTime"
+import isoTimeFormat from "../lib/isoTimeFormat"
+import dateFormat from "../lib/dateFormat"
 
 const MyBookings = () => {
-  return (
-    <div>MyBookings</div>
+
+  const currency = import.meta.env.VITE_CURRENCY
+  const [isLoading,setIsLoading] = useState(true)
+  const [bookings,setBookings] = useState([])
+
+  const getMyBookings = () => {
+    setBookings(dummyBookingData)
+    setIsLoading(false)
+  }
+
+  useEffect(()=>{
+    getMyBookings()
+  },[])
+
+  return !isLoading ? (
+    <div className="relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh] mt-8">
+      <h1 className="text-lg font-semibold mb-4">My Bookings</h1>
+      {/* Booked movie */}
+      {bookings.map((item,index)=>(
+        <div key={index} className="flex flex-col md:flex-row justify-between 
+         bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl">
+           <div className="flex flex-col md:flex-row">
+              <img src={item.show.movie.poster_path} alt="movie_poster" className="md:max-w-45 
+                aspect-video h-auto object-cover object-bottom rounded"/>
+              <div className="flex flex-col p-4">
+                <p className="text-lg font-semibold">{item.show.movie.title}</p>
+                <p className="text-gray-400 text-sm">{calcTime(item.show.movie.runtime)}</p>
+                <p className="text-gray-400 text-sm mt-auto">{`${dateFormat(item.show.showDateTime)} ${isoTimeFormat(item.show.showDateTime)}`}</p>
+              </div>  
+           </div>
+           <div className="flex flex-col md:items-end md:text-right justify-between p-4">
+              <div className="flex items-center gap-4">
+                <p className="text-2xl font-semibold mb-3">{currency}{item.amount}</p>
+                {!item.isPaid && <button className="bg-primary px-1 py-1.5 mb-3 text-sm 
+                 rounded-lg font-medium cursor-pointer active:scale-95">Pay Now</button>}
+              </div>
+              <div className="text-sm">
+                <p><span className="text-gray-300">Total Tickets: </span>{item.bookedSeats.length}</p>
+                <p><span className="text-gray-300">Seat Number: </span>{item.bookedSeats.join(",")}</p>
+              </div>
+           </div>
+        </div>
+      ))}
+    </div>
+  ):(
+    <Loading />
   )
 }
 
