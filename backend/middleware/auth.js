8cbc8ae620f7276fbc69514d2685103eb1c10302
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-12-16 10:12:44 +0530

    Added Admin middleware for Admin Routes

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+import {clerkClient} from "@clerk/express"
+
+export const protectAdmin = async (req,res,next) => {
+    try {
+        const {userId}= req.auth() // this auth() comes from clerk Middleware
+        if (!userId){
+          return res.json({success:false, message: "Not authenticated" });
+        }
+
+        const user = await clerkClient.users.getUser(userId)
+        if(user.privateMetadata.role !== "admin"){
+            return res.json({success:false, message:"Not Authorized"})
+        }
+        next()
+    } catch (error) {
+        return res.json({success:false, message:"Not Authorized"})
+    }
+}
