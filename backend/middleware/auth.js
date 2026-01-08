6e70a061be5e122ff269d1e6b4fb947fe98abd6f
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2026-01-08 16:56:48 +0530

    Integrated Admin AddShow feature with backend

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,16 +3,16 @@ import {clerkClient} from "@clerk/express"
 export const protectAdmin = async (req,res,next) => {
     try {
         const {userId}= req.auth() // this auth() comes from clerk Middleware
-        if (!userId){
-          return res.json({success:false, message: "Not authenticated" });
+        if(!userId){
+          return res.json({success:false, message: "Not authenticated(user not exist)" });
         }
 
         const user = await clerkClient.users.getUser(userId)
         if(user.privateMetadata.role !== "admin"){
-            return res.json({success:false, message:"Not Authorized"})
+            return res.json({success:false, message:"Not Authorized(not admin)"})
         }
         next()
     } catch (error) {
-        return res.json({success:false, message:"Not Authorized"})
+        return res.json({success:false, message:"Not Authorized(server error)"})
     }
 }
