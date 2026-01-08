0b9cce9991add195b336a1b250e54ce213c7e58e
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2026-01-08 21:26:28 +0530

    Integrated Admin List-shows service with backend

--- frontend/src/pages/admin/ListShows.jsx
+++ frontend/src/pages/admin/ListShows.jsx
@@ -1,31 +1,27 @@
 import { useEffect, useState } from "react"
-import { dummyShowsData } from "../../assets/assets"
 import Loading from "../../components/Loading"
 import Title from "../../components/admin/Title"
 import dateFormat from "../../lib/dateFormat"
 import isoTimeFormat from "../../lib/isoTimeFormat"
+import { useAppContext } from "../../context/AppContext"
 
 const ListShows = () => {
+
+  const {user, axios, getToken} = useAppContext()
 
   const currency = import.meta.env.VITE_CURRENCY
 
   const [loading,setLoading] = useState(true)
   const [shows,setShows] = useState([])
 
-  const fetchAllShows = () => {
+  const fetchAllShows = async() => {
     try {
-      setShows([
-        {
-          movie:dummyShowsData[0],
-          showDateTime: "2025-06-30T02:30:00.000Z",
-          showPrice: 59,
-          occupiedSeats:{
-            A1: "user_1",
-            B1: "user_2",
-            C1: "user_3"
-          }
+      const {data} = await axios.get("/api/admin/all-shows",{
+        headers:{
+          Authorization:`Bearer ${await getToken()}`
         }
-      ])
+      })
+      setShows(data.shows)
       setLoading(false)
     } catch (error) {
       console.error(error)
@@ -33,8 +29,10 @@ const ListShows = () => {
   }
 
   useEffect(()=>{
-    fetchAllShows()
-  },[])
+    if(user){
+      fetchAllShows()
+    }
+  },[user])
 
   return !loading ? (
     <>
